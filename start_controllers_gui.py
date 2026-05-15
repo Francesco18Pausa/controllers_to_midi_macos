3faef9d1e67e765df02bad9195e307a2e7b80947
Francesco <francesco@MacBookAir.homenet.telecomitalia.it>
2026-05-15 12:20:02 -0700

    PS4 Script works and GUI works, integrating new mapping method and editor

--- start_controllers_gui.py
+++ start_controllers_gui.py
@@ -1,39 +1,11 @@
-import subprocess
-import sys
-import threading
-import time
 import tkinter as tk
 from gui.launcher_gui import LauncherGUI
-
-# Percorso loopMIDI
-LOOPMIDI_PATH = r"C:\Program Files (x86)\Tobias Erichsen\loopMIDI\loopMIDI.exe"
-
-
-# ----------------------------------------------------
-# AVVIO loopMIDI
-# ----------------------------------------------------
-def start_loopmidi():
-    try:
-        subprocess.Popen(
-            [LOOPMIDI_PATH],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL
-        )
-        print("loopMIDI avviato")
-    except Exception as e:
-        print(f"Errore avvio loopMIDI: {e}")
 
 
 # ----------------------------------------------------
 # MAIN
 # ----------------------------------------------------
 def main():
-    # Avvio loopMIDI in background
-    threading.Thread(target=start_loopmidi, daemon=True).start()
-
-    # Attesa per permettere a loopMIDI di creare la porta
-    time.sleep(3)
-
     # Avvio GUI principale
     root = tk.Tk()
     app = LauncherGUI(root)
